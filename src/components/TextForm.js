f3f540d92f17591cd601be5c80772eaa559e1d96
Karan0811 <46247163+Karan0811@users.noreply.github.com>
2021-08-19 11:03:30 +0530

    word, letter count and preview text logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,12 +3,24 @@ import { useState } from 'react';
 
 export default function TextForm(props) {
     const [text,setText] = useState("Hello World")
+    // const [word,setWord] = useState((text.split(' ')).length)
+    // const [letter,setLetter] = useState(text.length)
+
     let changeToUpperCase = () => {
         let newText = text.toUpperCase()
         setText(newText)
     }
+    let changeToLowerCase = () => {
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
     let handleChange = (event) => {
-        setText(event.target.value)
+        let text = event.target.value
+        setText(text)
+        // let letterLength = (text).length
+        // let wordLength = (text.split(' ')).length
+        // setLetter(letterLength)
+        // setWord(wordLength)
     }
   return (
     <>
@@ -23,7 +35,19 @@ export default function TextForm(props) {
           rows='8'
         ></textarea>
       </div>
-      <button type="button" className="btn btn-primary" onClick={changeToUpperCase}>UpperCase</button>
+      <button type="button" className="btn btn-primary mx-2" onClick={changeToUpperCase}>UpperCase</button>
+      <button type="button" className="btn btn-primary" onClick={changeToLowerCase}>LowerCase</button>
+      </div>
+      <div className="container my-3">
+      <p>Word Count = {(text.split(' ')).length}</p>
+      <p>Letter Count = {(text).length}</p>
+      <p>Time to Read = {(text.split(' ')).length * 0.008}</p>
+      </div>
+      <div className="container my-3">
+      <h3>
+          Preview Text
+      </h3>
+      <p>{text}</p>
       </div>
     </>
   );
